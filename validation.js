1f35bae38e5a22507ee6aadf136cf704f25a8b66
malik dinar <malikdinaras@gmail.com>
2022-08-07 20:02:31 +0530

    final file

---
+++ validation.js
@@ -0,0 +1,44 @@
+function isname(){
+    var y = document.getElementById("fName").value;
+    var z = 0;
+    z = /\d/.test(y);
+    if (z == 0 )
+    {
+        document.getElementById("nameOneWarning").innerHTML= ""
+    }
+    else{
+        document.getElementById("nameOneWarning").innerHTML= "invalid name"
+    }
+    
+}
+
+function issname(){
+    var letters = /^[A-Za-z]+$/;
+    var y = document.getElementById("fName").value;
+    z=y.length
+    if(z == 0)
+    {
+        document.getElementById("nameOneWarning").innerHTML= "name cannot be empty"
+    } 
+    else if(/^[A-Za-z ]+$/.test(y))
+    {
+        document.getElementById("nameOneWarning").innerHTML= ""
+    }
+    else{
+        document.getElementById("nameOneWarning").innerHTML= "invalid name"
+    }   
+}
+
+function ValidateEmail() 
+{
+    var y = document.getElementById("email").value;
+    var len = y.length;
+    
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(y) || len == 0)
+    {
+        document.getElementById("emailWarning").innerHTML=""
+    }
+    else{
+        document.getElementById("emailWarning").innerHTML="Enter email format correctly"
+    }
+}
